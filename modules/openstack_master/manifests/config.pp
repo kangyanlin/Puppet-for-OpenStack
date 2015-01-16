b218388dac885de708e914c7724f21284abe6c68
李子延 <ziyan.li@cs2c.com.cn>
2015-01-16 19:21:14 +0800

    set sql pw & keystone connection & create/grant keystone db test

--- modules/openstack_master/manifests/config.pp
+++ modules/openstack_master/manifests/config.pp
@@ -1,5 +1,5 @@
 class openstack_master::config{
-  include net_config, sql_config, mess_config
+  include net_config, sql_config, mess_config, keystone_config
 }
 
 class net_config{
@@ -24,6 +24,24 @@ class sql_config{
     require => Class['sql_install'],
     notify  => Class['sql_service'],
   }
+  exec { 'set_mysql_password':
+    unless  => 'mysqladmin -uroot -pzeastion status',
+    path    => '/usr/bin',
+    command => 'mysqladmin -u root password "zeastion"',
+    require => Class['sql_service'],
+  }
+  file { 'keystone_sql.sh':
+    ensure  => present,
+    mode    => '755',
+    path    => '/tmp/keystone_sql.sh',
+    source  => 'puppet:///modules/openstack_master/keystone_sql.sh',
+  }
+  exec { 'set_database_keystone':
+    onlyif  => '/usr/bin/mysql -uroot -pzeastion',
+    unless  => '/usr/bin/mysql -ulzy -plzy',
+    path    => '/bin',
+    command => 'sh /tmp/keystone_sql.sh',
+  }
 }
 
 class mess_config{
@@ -35,5 +53,14 @@ class mess_config{
     mode    => '0644',
     require => Class['mess_install'],
     notify  => Class['mess_service'],
+  }
+}
+
+class keystone_config{
+  exec { 'set_keystone_connection':
+    require => Class['keystone_install'],
+    path    => "/usr/bin",
+    command => 'openstack-config --set /etc/keystone/keystone.conf database connection mysql://keystone:keystone@localhost/keystone && /bin/touch /tmp/keystone.my',
+    creates => "/tmp/keystone.my",
   }
 }
